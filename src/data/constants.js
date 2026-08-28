8746609b751459c68edb623df53dde889305472e
Vergil <asca123321@gmail.com>
2026-08-27 19:57:52 -0600

    chore(contact): update phone number to 5549058687

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,11 +2,11 @@ export const DOCTOR_CONFIG = {
   name: "Miriam Cabañas Alvistegui",
   title: "Dra.",
   experience: 22,
-  phoneRaw: "5622151939",
-  phoneIntl: "525622151939",
-  phoneDisplay: "+52 56 2215 1939",
+  phoneRaw: "5549058687",
+  phoneIntl: "525549058687",
+  phoneDisplay: "+52 55 4905 8687",
   whatsappUrl:
-    "https://wa.me/525622151939?text=Hola%20Dra.%20Miriam%2C%20me%20interesa%20agendar%20una%20consulta.%20%C2%BFMe%20podr%C3%ADa%20dar%20m%C3%A1s%20informaci%C3%B3n%3F",
+    "https://wa.me/525549058687?text=Hola%20Dra.%20Miriam%2C%20me%20interesa%20agendar%20una%20consulta.%20%C2%BFMe%20podr%C3%ADa%20dar%20m%C3%A1s%20informaci%C3%B3n%3F",
   email: "contacto@coramyr.mx",
 };
 
